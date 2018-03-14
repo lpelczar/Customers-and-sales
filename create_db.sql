492f4e1be5017ce8831640f4df452e26108c9809
lpelczar <lukepelczar@gmail.com>
2018-03-14 12:07:36 +0100

    Modify creating tables script

--- create_db.sql
+++ create_db.sql
@@ -2,7 +2,7 @@ CREATE TABLE customers (
   id INTEGER PRIMARY KEY,
   name TEXT,
   surname TEXT,
-  birthyear TEXT,
+  birthyear INTEGER,
   gender TEXT
 );
 
@@ -10,7 +10,7 @@ CREATE TABLE sales (
   id INTEGER ,
   customer_id INTEGER,
   product_name TEXT,
-  net_value TEXT,
-  tax_rate TEXT,
+  net_value REAL,
+  tax_rate INTEGER,
   FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
