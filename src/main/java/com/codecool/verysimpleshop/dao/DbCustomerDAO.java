cb9ceec8d1070ec89ef10d67af59a6af5a48468e
lpelczar <lukepelczar@gmail.com>
2018-03-14 17:54:31 +0100

    Add getting one customer from db

--- src/main/java/com/codecool/verysimpleshop/dao/DbCustomerDAO.java
+++ src/main/java/com/codecool/verysimpleshop/dao/DbCustomerDAO.java
@@ -5,6 +5,7 @@ import com.codecool.verysimpleshop.data.PreparedStatementCreator;
 import com.codecool.verysimpleshop.data.statements.CustomerStatement;
 import com.codecool.verysimpleshop.models.Customer;
 
+import java.awt.print.Book;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,10 +19,36 @@ public class DbCustomerDAO extends DbHelper implements CustomerDAO {
     private CustomerStatement customerStatement = new CustomerStatement();
 
     @Override
+    public Customer getById(int id) {
+        String sqlStatement = customerStatement.selectById();
+        Customer customer = null;
+        try {
+            PreparedStatement statement = getPreparedStatement(sqlStatement);
+            statement.setInt(1, id);
+            ResultSet resultSet = query(statement);
+            while (resultSet.next())
+                customer = new Customer(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("surname"),
+                        resultSet.getInt("birthyear"),
+                        resultSet.getString("gender")
+                );
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return customer;
+    }
+
+    @Override
     public List<Customer> getBySearchPhrase(String searchPhrase) {
 
         String sqlStatement = customerStatement.selectBySearchPhrase();
-        PreparedStatement statement = psc.getPreparedStatementBy(Collections.nCopies(4, "%" + searchPhrase + "%"),
+        PreparedStatement statement = psc.getPreparedStatementBy(Collections.nCopies(5, "%" + searchPhrase + "%"),
                 sqlStatement);
 
         List<Customer> customers = new ArrayList<>();
