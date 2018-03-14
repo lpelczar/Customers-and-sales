0cf305a37dbc399bb62d78e13db052c9afecbc25
lpelczar <lukepelczar@gmail.com>
2018-03-14 16:46:29 +0100

    Add customer model

---
+++ src/main/java/com/codecool/verysimpleshop/models/Customer.java
@@ -0,0 +1,24 @@
+package com.codecool.verysimpleshop.models;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private String surname;
+    private int birthYear;
+    private String gender;
+
+    public Customer(int id, String name, String surname, int birthYear, String gender) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.birthYear = birthYear;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " Name: " + name + " Surname: " + surname + " Birthyear: " + birthYear +
+                " Gender: " + gender;
+    }
+}
